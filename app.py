00ee4d9083ee8df7408266a214edd62cfa6965d2
Tithaye Beri <tithaye.beri@gmail.com>
2025-08-16 22:05:53 +0530

    fix download

--- app.py
+++ app.py
@@ -7,15 +7,14 @@ from nltk.stem.porter import PorterStemmer
 import os
 import pickle
 
+nltk_data_path = os.path.join(os.path.expanduser("~"), "nltk_data")
+if not os.path.exists(nltk_data_path):
+    os.makedirs(nltk_data_path)
 
-nltk_data_dir = os.path.join(os.path.dirname(__file__), "nltk_data")
+nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('stopwords', download_dir=nltk_data_path)
 
-os.makedirs(nltk_data_dir, exist_ok=True)
-
-nltk.download('punkt', download_dir=nltk_data_dir, quiet=True)
-nltk.download('stopwords', download_dir=nltk_data_dir, quiet=True)
-
-nltk.data.path.append(nltk_data_dir)
+nltk.data.path.append(nltk_data_path)
 
 
 # Initialize stemmer
